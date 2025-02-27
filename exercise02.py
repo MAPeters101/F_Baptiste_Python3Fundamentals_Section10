57d306a84cedaa0ca47e5e3ffb718256fd1ffd77
Mark Peters <mapeters101@earthlink.net>
2025-02-26 23:44:06 -0500

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,19 @@
+'''
+Question 2
+Find all the numbers from 1 to 100 (inclusive) that are divisible by any single
+digit except 1.
+
+For example, 22 is divisible by 2, so it should get included in the result.
+But 11 is not divisible by any of the digits 2-9, so it should not get included.
+
+Your solution should use comprehensions. (Hint: you may need nested loops in
+your comprehension)
+
+Next, find all the numbers from 1 to 100 (inclusive) that are not in the list
+of numbers you just generated.
+
+If your code works correctly, you should end up with this result:
+
+{1, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
+83, 89, 97}
+'''
